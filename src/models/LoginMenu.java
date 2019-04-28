9a347873012878c8d8cec35a6d47376e56d5f324
erfanfi79 <45295258+erfanfi79@users.noreply.github.com>
2019-04-28 09:18:36 +0430

    basic class

---
+++ src/models/LoginMenu.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.util.ArrayList;
+
+public class LoginMenu {
+    private ArrayList<Account> users;
+
+    public void createAccount(String userName, String passWord) {
+
+    }
+
+    public void checkIfAccountExist(String userName) {
+    }
+
+    public void login(String userName, String passWord) {
+    }
+
+    public void getLeaderBoard() {
+    }
+
+    public void save(Account account) {
+    }
+
+    public void logout(Account account) {
+    }
+
+    public static String findUserFolder(Account account) {
+    }
+
+}
---
+++ src/models/Shop.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Shop {
+    private ArrayList<Card> cards;
+    private ArrayList<Item> items;
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void buyItem(Item item, Account account) {
+    }
+
+    public void buyCard(Card card, Account account) {
+    }
+
+    public void sellCard(Card card, Account account) {
+    }
+
+    public void sellItem(Item item, Account account) {
+    }
+
+    private void sortCards() {
+    }
+
+    private void initializeItem() {
+    }
+
+    private void initializeCard() {
+    }
+}
