9fd4f9823328820c1451ef18a2679d84a8b4c2b6
ahak14 <ah.abbasi.ah@gmail.com>
2019-04-29 21:20:16 +0430

    jar files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import view.battleView.BattleView;
+import view.battleView.GameInfoBattleViewMode1;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("first commit");
+
+        GameInfoBattleViewMode1 a = new GameInfoBattleViewMode1();
+        a.setPlayer1HeroHP(10);
+        a.setPlayer2HeroHP(10);
+        a.setPlayer1Mana(20);
+        a.setPlayer2HeroHP(20);
+
+        BattleView b = new BattleView();
+        b.show(a);
     }
 }
