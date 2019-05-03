b1523a23ea801f14227d32ecd2846e1c03319538
Ali5859 <abolhassaneslami@gmail.com>
2019-05-03 13:03:15 +0430

    buff changed

--- src/models/Buff.java
+++ src/models/Buff.java
@@ -10,12 +10,16 @@ public class Buff {
     private boolean stun;
     private boolean disarm;
     private int unholy;
-
+    private int cancelBuff;
+    private boolean applyWhenTurnEnds;
+    private boolean lasts;
     public void decrementDuration() {
         duration -= 1;
     }
 
-    public Buff(int duration, int holy, int power, int poison, int weaknessAP, int weaknessHP, boolean stun, boolean disarm, int unholy) {
+    public Buff(int duration, int holy, int power, int poison, int weaknessAP,
+                int weaknessHP, boolean stun, boolean disarm,
+                int unholy, int cancelBuff,boolean applyWhenTurnEnds, boolean lasts) {
         this.duration = duration;
         this.holy = holy;
         this.power = power;
@@ -25,6 +29,9 @@ public class Buff {
         this.stun = stun;
         this.disarm = disarm;
         this.unholy = unholy;
+        this.cancelBuff = cancelBuff;
+        this.applyWhenTurnEnds = applyWhenTurnEnds;
+        this.lasts = lasts;
     }
 
     public int getUnholy() {
@@ -41,6 +48,10 @@ public class Buff {
 
     public int getDuration() {
         return duration;
+    }
+
+    public boolean isLasts() {
+        return lasts;
     }
 
     public int getHoly() {
@@ -61,5 +72,13 @@ public class Buff {
 
     public int getPoison() {
         return poison;
+    }
+
+    public int getCancelBuff() {
+        return cancelBuff;
+    }
+
+    public boolean isApplyWhenTurnEnds() {
+        return applyWhenTurnEnds;
     }
 }
